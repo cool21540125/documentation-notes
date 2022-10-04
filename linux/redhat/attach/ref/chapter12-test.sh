153546402745955f33f3e923807f67ca22c16e77
Tony Chou <cool21540125@gmail.com>
2022-10-04 22:45:58 +0800

    rm rhce

--- linux/redhat/attach/ref/chapter12-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in $(seq 3) ; do
- ping -c1 foundation${i} &> /dev/null && \
- echo "Begin remote installation on foundation${i}." || \
- echo 'Keep foundation${i} failed record in text file!'
-done
