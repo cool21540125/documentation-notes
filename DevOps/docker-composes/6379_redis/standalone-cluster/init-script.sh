12542cee72d4718366a75f082f90cd08acdaf789
TonyChou <tonychou@wondercise.com>
2023-05-16 18:29:22 +0800

    redis-cluster (未完成)

--- DevOps/docker-composes/6379_redis/standalone-cluster/init-script.sh
+++ DevOps/docker-composes/6379_redis/standalone-cluster/init-script.sh
@@ -1,2 +1,6 @@
 
-docker network create net_rediscluster
+### 須先確保主機所在網路, 該網段沒有在使用
+docker network create --subnet 172.72.72.0/24 net_rediscluster
+
+### 下列的 docker container IP 需要修改
+redis-cli --cluster create redis01:6001 redis02:6002 redis03:6003 redis04:6004 redis05:6005 redis06:6006 --cluster-replicas 1
