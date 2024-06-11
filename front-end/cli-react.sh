403eb50144d88da9ed0d1fafe79f0883eabb95ca
TonyChou <cool21540125@gmail.com>
2024-06-11 17:28:46 +0800

    cli - react

---
+++ front-end/cli-react.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+exit 0
+# -------------
+
+
+### New React App
+npx create-react-app $PROJECT
+npx create-react-app $PROJECT --template typescript
+
+
+### create new component for react app
+npx generate-react-cli component /PATH/TO/$COMPONENT
