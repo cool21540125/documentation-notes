f4fa56cc861b2da66ea583f46fa6682b1721d203
TonyChou <cool21540125@gmail.com>
2025-06-28 18:17:43 +0800

    java misc

--- Programming/java/cli_javac.sh
+++ Programming/java/cli_javac.sh
@@ -15,3 +15,20 @@ javac -sourcepath SRC.java
 javac -d $PATH_TO_CLASS_OUTPUT_DIR
 
 ### ========================================= 輔助查詢功能 =========================================
+
+### -------------------------------------- Compile time 的疑難雜症 --------------------------------------
+javac FixedCapacityStack.java
+#Note: FixedCapacityStack.java uses unchecked or unsafe operations.
+#Note: Recompile with -Xlint:unchecked for details.
+# 由於使用了 Generics, 但沒有指定具體的類型, 所以會有 unchecked warning
+
+## 加入提示...
+javac -Xlint:unchecked FixedCapacityStack.java
+#FixedCapacityStack.java:26: warning: [unchecked] unchecked cast
+#found : java.lang.Object[]
+#required: Item[]
+#        a = (Item[]) new Object[capacity];
+#                     ^
+#1 warning
+
+### =========================================
